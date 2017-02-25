91bfb2853e05e5ee7579ab691eec14cef266f977
Caleb Gregory <calebgregory@github.com>
2017-02-24 19:32:39 -0500

    Yea..

---
+++ app/export.js
@@ -0,0 +1,4 @@
+const obj = { a: 'A', b: 'B' };
+
+export const a = object.a;
+export const b = object.b;
---
+++ app/index.js
@@ -0,0 +1,4 @@
+import { a, b } from './export';
+
+console.log('a', a);
+console.log('b', b);
